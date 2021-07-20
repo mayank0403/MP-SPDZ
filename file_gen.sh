3eee1f957dca65f427c69712f7a1f6dbdd007117
Mayank Rathee <mayankrathee.japan@gmail.com>
2021-07-20 00:17:12 -0700

    Scripts minor

--- file_gen.sh
+++ file_gen.sh
@@ -23,7 +23,7 @@ do
     ./compile.py -Z 3 -R 128 "dorydb_mthread_""$i""_""$3""_""$4""_$5"".mpc"
     echo "Compiling Mal Multithreaded file ..."
     cp "Programs/Source/dorydb_mthread_""$i""_""$3""_""$4""_$5"".mpc" "Programs/Source/dorydb_mthread_mal_""$i""_""$3""_""$4""_$5"".mpc"
-    ./compile.py -Z 3 -R 88 "dorydb_mthread_mal_""$i""_""$3""_""$4""_$5"".mpc"
+    ./compile.py -Z 3 -R 72 "dorydb_mthread_mal_""$i""_""$3""_""$4""_$5"".mpc"
     
     echo "Generating Round-optimal file ..."
     python3 generate_file.py "Programs/Source/dorydb_roundopt_template.mpc" "Programs/Source/dorydb_roundopt_""$i""_""$3""_""$4""_$5"".mpc" "$i" "$3" "$4" "$5"
@@ -31,6 +31,7 @@ do
     ./compile.py -Z 3 -R 128 "dorydb_roundopt_""$i""_""$3""_""$4""_$5"".mpc"
     echo "Compiling Mal Round-optimal file ..."
     cp "Programs/Source/dorydb_roundopt_""$i""_""$3""_""$4""_$5"".mpc" "Programs/Source/dorydb_roundopt_mal_""$i""_""$3""_""$4""_$5"".mpc"
-    ./compile.py -Z 3 -R 88 "dorydb_roundopt_mal_""$i""_""$3""_""$4""_$5"".mpc"
+    ./compile.py -Z 3 -R 72 "dorydb_roundopt_mal_""$i""_""$3""_""$4""_$5"".mpc"
 done
 
+# using 72 bits for malicious because 128 - 40 = 88 requires recompilation of the whole VM
