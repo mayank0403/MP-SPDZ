1a7dc2226fd3edf97730721ddc309ca705e0c173
Mayank Rathee <mayankrathee.japan@gmail.com>
2021-07-28 13:25:30 -0700

    Scripts more

--- special-group.sh
+++ special-group.sh
@@ -3,3 +3,11 @@
 echo "MOD = -DRING_SIZE=88" >> CONFIG.mine
 
 echo "Set MP-SPDZ bitwidth of 88"
+echo "Recompiling VMs"
+
+make clean
+make -j replicated-ring-party.x
+make -j ps-rep-ring-party.x
+
+echo "Recompiled"
+
